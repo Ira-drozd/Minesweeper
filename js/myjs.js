054703f9e781454a92ed48e6c58ba392e5571879
Ira <moby.dick.killer@gmail.com>
2019-11-17 00:38:18 +0300

    new grid

--- js/myjs.js
+++ js/myjs.js
@@ -3,6 +3,82 @@ function createGrid(row, col) {
     for (let i = 0; i < row; i++) {
         grid[i] = [];
         for (let j = 0; j < col; j++) {
+            grid[i][j] = 0;
+        }
+    }
+
+    for (let i = 0; i < grid.length; i++) {
+        for (let j = 0; j < grid.length; j++) {
+            if (i == j) {
+                grid[i][j] = 'b';
+                grid.sort(function () {
+                    return Math.random() - 0.5;
+                });
+            }
+        }
+    }
+
+    for (let i = 0; i < grid.length; i++) {
+        grid[i].push(0);
+        grid[i].unshift(0);
+    }
+
+    let nullRow=[];
+    for(let r=0; r<row+2; r++){
+        nullRow[r]=0;
+    }
+    grid.push(nullRow);
+    grid.unshift(nullRow);
+
+    console.log(grid);
+    // generateHelp(grid);
+    //printGrid(grid);
+
+    return grid;
+
+}
+
+
+/*
+function createGrid(row, col) {
+    let grid = [];
+    for (let i = 0; i < row; i++) {
+        grid[i] = [];
+        for (let j = 0; j < col; j++) {
+            grid[i][j] =0;
+
+        }
+    }
+
+
+
+    for (let i = 0; i < grid.length; i++) {
+grid[i].push(8);
+        grid[i].unshift(8);
+        for (let j = 0; j < grid.length; j++) {
+
+        }
+    }
+
+    let nullRow=[];
+    for(let r=0; r<row+2; r++){
+        nullRow[r]=0;
+    }
+    grid.push(nullRow);
+    grid.unshift(nullRow);
+
+
+    console.log(grid);
+
+}
+*/
+
+
+/*function createGrid(row, col) {
+    let grid = [];
+    for (let i = 0; i < row; i++) {
+        grid[i] = [];
+        for (let j = 0; j < col; j++) {
 
             grid[i][j] = 0;
 
@@ -26,7 +102,7 @@ function createGrid(row, col) {
     // console.log(grid);
     return grid;
 
-}
+}*/
 
 function generateHelp(grid) {
 
@@ -135,6 +211,113 @@ function generateHelp(grid) {
     return grid;
 }
 
+/*function generateHelp(grid) {
+
+    //right
+    for (let i = 0; i < grid.length; i++) {
+        for (let j = 0; j < grid.length; j++) {
+            if (grid[i][j] == 'bomb') {
+                let counter;
+                //right
+                if (j + 1 < grid.length) { //&& j-1>-1 && i-1>-1 && i + 1<grid.length
+
+                    counter = grid[i][j + 1];
+                    if (grid[i][j + 1] == 'bomb') {
+                        grid[i][j + 1] = counter;
+                    } else {
+                        grid[i][j + 1] = counter + 1;
+                    }
+                    //right-top
+                    if (i - 1 > -1) {
+                        counter = grid[i - 1][j + 1];
+                        if (grid[i][j] == 'bomb') {
+
+                            if (grid[i - 1][j + 1] == 'bomb') {
+                                grid[i - 1][j + 1] = counter;
+                            } else {
+                                grid[i - 1][j + 1] = counter + 1;
+                            }
+                        }
+                    }
+                    //right-bottom
+                    if (i + 1 < grid.length) {
+                        counter = grid[i + 1][j + 1];
+                        if (grid[i][j] == 'bomb') {
+
+                            if (grid[i + 1][j + 1] == 'bomb') {
+                                grid[i + 1][j + 1] = counter;
+                            } else {
+                                grid[i + 1][j + 1] = counter + 1;
+                            }
+                        }
+                    }
+                }
+                //left
+                if (j - 1 > -1) {
+
+                    counter = grid[i][j - 1];
+                    if (grid[i][j - 1] == 'bomb') {
+                        grid[i][j - 1] = counter;
+                    } else {
+                        grid[i][j - 1] = counter + 1;
+                    }
+                    //left-top
+                    if (i - 1 > -1) {
+                        counter = grid[i - 1][j - 1];
+                        if (grid[i][j] == 'bomb') {
+
+                            if (grid[i - 1][j - 1] == 'bomb') {
+                                grid[i - 1][j - 1] = counter;
+                            } else {
+                                grid[i - 1][j - 1] = counter + 1;
+                            }
+                        }
+                    }
+                    //left-bottom
+                    if (i + 1 < grid.length) {
+                        counter = grid[i + 1][j - 1];
+                        if (grid[i][j] == 'bomb') {
+
+                            if (grid[i + 1][j - 1] == 'bomb') {
+                                grid[i + 1][j - 1] = counter;
+                            } else {
+                                grid[i + 1][j - 1] = counter + 1;
+                            }
+                        }
+                    }
+                }
+                //top
+                if (i - 1 > -1) {
+                    counter = grid[i - 1][j];
+                    if (grid[i][j] == 'bomb') {
+
+                        if (grid[i - 1][j] == 'bomb') {
+                            grid[i - 1][j] = counter;
+                        } else {
+                            grid[i - 1][j] = counter + 1;
+                        }
+
+                    }
+                }
+                //bottom
+                if (i + 1 < grid.length) {
+                    counter = grid[i + 1][j];
+                    if (grid[i][j] == 'bomb') {
+
+                        if (grid[i + 1][j] == 'bomb') {
+                            grid[i + 1][j] = counter;
+                        } else {
+                            grid[i + 1][j] = counter + 1;
+                        }
+                    }
+                }
+
+            }
+        }
+    }
+    return grid;
+}*/
+
 function printGrid(grid) {
     console.log(grid);
     const printG = document.querySelector('.grid');
@@ -156,10 +339,10 @@ function printGrid(grid) {
 function stayFor() {
     let width = parseInt(document.getElementById("width").value);
     let height = parseInt(document.getElementById("height").value);
-    if (width && height) {
-        let grid = createGrid(width, height);
-        document.querySelector(".create-grid").style.display = "none";
-    }
+    //if (width && height) {
+    let grid = createGrid(10, 10);//!!!!!!!!!!
+    document.querySelector(".create-grid").style.display = "none";
+    // }
     // return false;
 }
 
@@ -169,9 +352,9 @@ document.getElementById("create-button").addEventListener("click", stayFor);
 
 function openDiv() {
     let divElem = document.querySelectorAll(".elem");
-    console.log(typeof (divElem));//collection obj set
+    //  console.log(typeof (divElem));//collection obj set
     for (let elem of divElem) {
-        console.log(typeof (elem));
+        //   console.log(typeof (elem));
         elem.addEventListener("click", function () {
             elem.style.backgroundColor = "#828282";
             if (elem.innerHTML == 1) {
@@ -183,13 +366,13 @@ function openDiv() {
             if (elem.innerHTML == 3) {
                 elem.style.color = 'red';
             }
-            if (elem.innerHTML=="bomb"){
-                elem.innerHTML="\uD83D\uDCA3";
+            if (elem.innerHTML == "bomb") {
+                elem.innerHTML = "\uD83D\uDCA3";
                 alert("Game over");
             }
-            if (elem.innerHTML==0){
-                elem.innerHTML=" ";
-                }
+            if (elem.innerHTML == 0) {
+                elem.innerHTML = " ";
+            }
 
         });
 
@@ -198,7 +381,6 @@ function openDiv() {
 }
 
 document.addEventListener("click", openDiv);
-
 
 
 function putFlag() {
@@ -219,7 +401,8 @@ function putFlag() {
 
     }
 }
-document.addEventListener('contextmenu',  putFlag);
+
+document.addEventListener('contextmenu', putFlag);
 
 
 
